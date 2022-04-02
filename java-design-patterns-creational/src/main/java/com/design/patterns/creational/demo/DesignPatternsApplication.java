b6a17289251643478ab8cf7f1d2c132ea5272946
lsefiane <lyes.sefiane@gmail.com>
2022-04-02 13:27:56 -0400

    ~ init

--- java-design-patterns-creational/src/main/java/com/design/patterns/creational/demo/DesignPatternsApplication.java
+++ java-design-patterns-creational/src/main/java/com/design/patterns/creational/demo/DesignPatternsApplication.java
@@ -1,25 +1,9 @@
 package com.design.patterns.creational.demo;
 
-import java.awt.Color;
-import java.awt.Dimension;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.design.patterns.creational.abstractfactory.AbstractFactory;
-import com.design.patterns.creational.abstractfactory.FactoryProvider;
-import com.design.patterns.creational.abstractfactory.FactoryType;
-import com.design.patterns.creational.abstractfactory.LollipopFactory;
-import com.design.patterns.creational.builder.Bedroom;
-import com.design.patterns.creational.builder.BedroomBuilder;
-import com.design.patterns.creational.factorymethod.CandyType;
-import com.design.patterns.creational.factorymethod.ChocolateFactory;
-import com.design.patterns.creational.prototype.Breed;
-import com.design.patterns.creational.prototype.Person;
-import com.design.patterns.creational.prototype.Rabbit;
-import com.design.patterns.creational.singleton.PrintSpooler;
 
 /**
  * 
--- java-design-patterns-structural/src/main/java/com/design/patterns/structural/demo/DesignPatternsApplication.java
+++ java-design-patterns-structural/src/main/java/com/design/patterns/structural/demo/DesignPatternsApplication.java
@@ -1,37 +1,9 @@
 package com.design.patterns.structural.demo;
 
-import java.nio.file.Path;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.design.patterns.structural.adapter.Adapter;
-import com.design.patterns.structural.adapter.CarModel;
-import com.design.patterns.structural.adapter.CarPriceCalculator;
-import com.design.patterns.structural.adapter.PriceCalculator;
-import com.design.patterns.structural.adapter.TruckPriceCalculator;
-import com.design.patterns.structural.adapter.lib.UKCarPriceCalculator;
-import com.design.patterns.structural.bridge.Blue;
-import com.design.patterns.structural.bridge.Circle;
-import com.design.patterns.structural.bridge.Red;
-import com.design.patterns.structural.bridge.Shape;
-import com.design.patterns.structural.bridge.Square;
-import com.design.patterns.structural.composite.Manager;
-import com.design.patterns.structural.composite.SalesPerson;
-import com.design.patterns.structural.composite.SalesTeam;
-import com.design.patterns.structural.decorator.BasicCar;
-import com.design.patterns.structural.decorator.Car;
-import com.design.patterns.structural.decorator.SportCarDecorator;
-import com.design.patterns.structural.facade.OrderServiceFacade;
-import com.design.patterns.structural.facade.OrderServiceFacadeImpl;
-import com.design.patterns.structural.facade.domain.Product;
-import com.design.patterns.structural.flyweight.Vehicle;
-import com.design.patterns.structural.flyweight.VehicleFactory;
-import com.design.patterns.structural.flyweight.VehicleType;
-import com.design.patterns.structural.proxy.virtual.Image;
-import com.design.patterns.structural.proxy.virtual.ImageProxy;
 
 /**
  * 
